4a3783d275c65d3bf0795b0e7f5a38912981a6f7
Ashley Maceli and Inthrayuth Mahaphol <ashley@>
2016-06-13 14:57:00 -0700

    add computed property to sort listings by price

--- app/components/category-detail.js
+++ app/components/category-detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['price:asc'],
+  sortedListings: Ember.computed.sort('category.listings', 'sortBy'),
+
   actions: {
     saveListing(listing, params) {
       this.sendAction('saveListing', listing, params);
