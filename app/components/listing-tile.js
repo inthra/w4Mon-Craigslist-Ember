3911978fb4479bffe37c11f17d7746b47d6ce63c
Ashley Maceli and Inthrayuth Mahaphol <ashley@>
2016-06-13 14:45:45 -0700

    add computed property for dislpaying listing title and price

--- app/components/listing-tile.js
+++ app/components/listing-tile.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  titleAndCost: Ember.computed('listing.title', 'listing.price', function() {
+    return this.get('listing.title') + ': $' + this.get('listing.price');
+  }),
 });
